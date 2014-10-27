0eefec50d4458aa51e6e5a48e53def6f5aaa13a0
Tom Scott <tscott@telvue.com>
2014-10-26 23:44:16 -0400

    1.9ify it

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,10 +1,10 @@
 if defined? ChefSpec
-  def execute_chruby_command(command, version: 'embedded')
+  def execute_chruby_command(command, version='embedded')
     ChefSpec::Matchers::ResourceMatcher.new \
       :execute, :run, "chruby-exec #{version} -- '#{command}'"
   end
 
-  def install_chruby_gem_package(name, version: 'embedded')
+  def install_chruby_gem_package(name, version='embedded')
     ChefSpec::Matchers::ResourceMatcher.new :gem_package, :install, name
   end
 
