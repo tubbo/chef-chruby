7d2e1ceeba106c7a6feb0b8dc22aa2aaac444683
Tom Scott <tscott@telvue.com>
2014-10-28 13:56:12 -0400

    Set user and group

--- providers/execute.rb
+++ providers/execute.rb
@@ -2,9 +2,13 @@ action :run do
   ruby_version = new_resource.ruby_version
   command = new_resource.command
   chruby_exec_command = "chruby-exec #{ruby_version} -- '#{command}'"
+  run_as_user = new_resource.user || ENV['USER']
+  run_as_group = new_resource.group || ENV['USER']
 
   execute new_resource.name do
-    cwd working_directory
+    cwd     working_directory
     command chruby_exec_command
+    user    run_as_user
+    group   run_as_group
   end
 end
--- resources/execute.rb
+++ resources/execute.rb
@@ -4,3 +4,5 @@ default_action :run
 attribute :ruby_version, kind_of: String
 attribute :command, kind_of: String, name_attribute: true
 attribute :cwd, kind_of: String
+attribute :user, kind_of: String
+attribute :group, kind_of: String
